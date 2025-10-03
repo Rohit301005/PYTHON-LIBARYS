c102ac52ae7124e4ddf2882e6b1a795de5769cb9
Rohit301005 <rohitkumarrrawat04@gmail.com>
2025-10-03 21:44:01 +0530

    properties

---
+++ Numpy Arrays Properties/ndim.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr1d = np.array([1,2,3])
+arr2d = np.array([[1,2,3],[4,5,6]])
+arr3d = np.array([[[1,2],[3,4],[5,6],[7,8]]])
+print(arr1d.ndim)
+print(arr2d.ndim)
+print(arr3d.ndim)
---
+++ Numpy Arrays Properties/size.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+ar2d = np.array([[1,2,3],[4,5,6]])
+print(ar2d.size)

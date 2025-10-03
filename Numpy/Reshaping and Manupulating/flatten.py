30479058ba21c0ff249947609d24f6040d551876
Rohit301005 <rohitkumarrrawat04@gmail.com>
2025-10-03 22:12:26 +0530

    reshape

---
+++ Numpy/Reshaping and Manupulating/flatten.py
@@ -0,0 +1,11 @@
+"""
+.revel() --> view
+.flatten() --> copy"""
+
+import numpy as np
+
+arr = np.array([[1,2,3],[4,5,6]])
+
+print(arr.ravel())
+print(arr.flatten())
+
---
+++ Numpy/Reshaping and Manupulating/reshape.py
@@ -0,0 +1,9 @@
+"""
+reshape(rows, column)
+if dimensions match"""
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6])
+reshaped_arr = arr.reshape(2,3)
+print(reshaped_arr)
